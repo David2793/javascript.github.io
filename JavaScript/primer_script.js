05551d9ce7d27cd1f64450528ed8359cfd2f16ec
Davif <sdavidantoniog@gmail.com>
2018-10-01 18:34:04 -0300

    ol

---
+++ JavaScript/primer_script.js
@@ -0,0 +1,10 @@
+var numIngresado= prompt("Bienvenid@, ingresa un número par");
+
+var valIngresado= parseInt(numIngresado);
+
+if (valIngresado % 2 === 0){
+    alert("El número ingresado es par!");
+}else{
+    alert("El número ingresado es impar!");
+}
+alert(numIngresado);
